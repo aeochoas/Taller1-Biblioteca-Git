eddb48dd5cbbe43d3e1647d924f14d68a7d0c87b
Alejandro Ochoa <aeochoa@unicesar.edu.co>
2026-08-18 11:01:04 -0500

    feat: Implementar READ de libro(listar)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -145,4 +145,17 @@ public class Main {
 
         System.out.println("Libro registrado con éxito.");
     }
+
+
+    
+    public static void listarLibros() {
+        System.out.println("=== Lista de libros ===");
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+            return;
+        }
+        for (Libro l : libros) {
+            System.out.println(l);
+        }
+    }
 }
